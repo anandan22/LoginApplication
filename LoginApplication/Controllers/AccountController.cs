384064d10dcea7064d3aab59e1e22a391cd11947
lidhish <lidhish@ANAND3BA1>
2021-11-19 00:03:04 +0000

    Add project files.

---
+++ LoginApplication/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using LoginApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginApplication.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+       public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if(ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View(loginViewModel);
+        }
+    }
+}
