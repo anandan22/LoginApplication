d68344c60ce0be93f5ec43bec0025861c1666109
anandan22 <anandhan.kanchi@gmail.com>
2021-11-21 12:39:16 +0000

    Added the connectionstring,EF,PasswordHash registeration in configurations methods

--- LoginApplication/Startup.cs
+++ LoginApplication/Startup.cs
@@ -1,3 +1,4 @@
+using LoginApplication.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -7,7 +8,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using ScottBrady91.AspNetCore.Identity;
 namespace LoginApplication
 {
     public class Startup
@@ -23,7 +26,25 @@ namespace LoginApplication
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+          
+
+            string connectionString = Configuration.GetConnectionString("default");
+
+            services.AddDbContext<AppDbContext>(c => c.UseSqlServer(connectionString));
+
+            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+
+            services.AddScoped<IPasswordHasher<IdentityUser>, BCryptPasswordHasher<IdentityUser>>();
+
+            services.Configure<BCryptPasswordHasherOptions>(options => {
+                options.WorkFactor = 11;
+                options.EnhancedEntropy = false;
+            });
+
+            services.AddControllersWithViews();
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -39,6 +60,8 @@ namespace LoginApplication
             app.UseStaticFiles();
 
             app.UseRouting();
+
+           app.UseAuthentication();
 
             app.UseAuthorization();
 
