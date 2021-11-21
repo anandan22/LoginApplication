039dfc972db62c22d9dff6cf4e5baa974cd62b03
anandan22 <anandhan.kanchi@gmail.com>
2021-11-21 12:40:30 +0000

    Added the DbContext

---
+++ LoginApplication/Models/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginApplication.Models
+{
+    public class AppDbContext : IdentityDbContext
+    {
+
+        private readonly DbContextOptions _options;
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+            _options = options;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
